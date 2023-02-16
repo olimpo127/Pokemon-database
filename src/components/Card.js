554c908435dd56f9ebc9b18b9db37427d57b344c
Felipe Ojeda <fojedai47@gmail.com>
2023-02-16 09:08:57 -0300

    home view 1.0

---
+++ src/components/Card.js
@@ -0,0 +1,27 @@
+const Card = ({ card, handleFavorite }) => {
+  return (
+    <div
+      style={{
+        flex: "0 0 calc(25.33% - 20px)",
+        margin: '40px',
+        border: '1px solid black',
+        background: 'white',
+        padding: '10px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center'
+      }}
+    >
+      <h2>{card.name}</h2>
+      <h4>#{card.id}</h4>
+      <img src={card.image} alt={card.name} style={{ width: '100%', height: '50%' }} />
+      <button onClick={() => handleFavorite(card.id, card.name)} style={{ background: "white", border: "none" }}>
+        <i className={card.favorite ? "fa-solid fa-heart text-danger fa-lg" : "fa-regular fa-heart fa-lg"}></i>
+      </button>
+      <button className='btn btn-primary mt-3'>Read more</button>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Navbar = ({ favoriteNames, showFavorites, handleShowFavorites, handleRemoveFavorite }) => {
+  const favoriteCount = favoriteNames.length;
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{ display: 'flex', alignItems: 'center' }}>
+      <a className="navbar-brand ms-3" href="http://localhost:3000/">HOME</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div style={{ display: 'flex', alignItems: 'center', marginLeft: 'auto', marginRight: '2rem' }}>
+        {showFavorites ? (
+          <div className='bg-dark'>
+            {favoriteNames.length > 0 ? (
+              <ul className="favorite-list">
+                {favoriteNames.map((favorite) => (
+                  <li key={favorite.id} className="favorite-item">
+                    {favorite.name}
+                    <button onClick={() => handleRemoveFavorite(favorite.id)} className="bg-danger"><i class="fa-solid fa-trash"></i></button>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p className="empty-list">Empty</p>
+            )}
+            <button className='btn btn-secondary mt-2' onClick={handleShowFavorites}>
+              Close Favorite Cards
+            </button>
+          </div>
+        ) : (
+          <button className='btn btn-success' onClick={handleShowFavorites}>
+            Favorite Cards ({favoriteCount})
+          </button>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
